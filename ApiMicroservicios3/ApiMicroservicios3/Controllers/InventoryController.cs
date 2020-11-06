41064dcedd37b21efe7b441e3b80f5f0ebc72ae2
Martin <m.felipe59@hotmail.com>
2020-11-05 19:25:32 -0500

    El api

---
+++ ApiMicroservicios3/ApiMicroservicios3/Controllers/InventoryController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiMicroservicios3.Controllers
+{
+    public class InventoryController
+    {
+    }
+}
---
+++ ApiMicroservicios3/ApiMicroservicios3/Models/BasicProductInfo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiMicroservicios3.Models
+{
+    public class BasicProductInfo
+    {
+    }
+}
