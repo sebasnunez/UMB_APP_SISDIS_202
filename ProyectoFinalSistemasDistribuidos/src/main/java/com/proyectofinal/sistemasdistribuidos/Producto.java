310d63c8e7d68f945e4a37fd98268c114b5cd541
edison sanchez <edisondeveloper@hotmail.com>
2020-11-08 21:13:41 -0500

    agregando funcionalidades al api

---
+++ ProyectoFinalSistemasDistribuidos/src/main/java/com/proyectofinal/sistemasdistribuidos/Producto.java
@@ -0,0 +1,48 @@
+package com.proyectofinal.sistemasdistribuidos;
+
+public class Producto {
+
+    private int id;
+    private String nombre;
+    private String descripcion;
+    private int precio;
+
+    public Producto(int id, String nombre, String descripcion, int precio){
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
